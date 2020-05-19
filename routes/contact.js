c486a9c207ef20b6750a1ddddff7c761d573d732
patrickmoy <patrickrnmoy@gmail.com>
2020-05-19 03:19:36 -0700

    Endpoints complete for webservice CRUD in contacts, chat, weather

--- routes/contact.js
+++ routes/contact.js
@@ -73,4 +73,133 @@ router.get("/", (req, res) => {
     }
 });
 
+router.delete("/:memberID_A/:memberID_B", (request, response, next) => {
+        //validate on empty parameters
+        if (!request.params.memberID_A || !request.params.memberID_B) {
+            response.status(400).send({
+                message: "Missing required information"
+            });
+        } else if ((isNaN(request.params.memberID_A) || isNaN(request.params.memberID_B))) {
+            response.status(400).send({
+                message: "Malformed parameter. Member ID must be a number"
+            });
+        } else {
+            next();
+        }
+    }, (request, response, next) => {
+        //validate chat id exists
+        let query = 'SELECT * FROM CONTACTS WHERE MemberID_A = $1 AND MemberID_B = $2';
+        let values = [request.params.memberID_A, request.params.memberID_B];
+        pool.query(query, values)
+            .then(result => {
+                if (result.rowCount == 0) {
+                    response.status(404).send({
+                        message: "Contact does not exist"
+                    });
+                } else {
+                    next();
+                }
+            }).catch(error => {
+            response.status(400).send({
+                message: "SQL Error",
+                error: error
+            });
+        })
+    }, (request, response) => {
+        //Delete the Contact from the table
+        let insert = `DELETE FROM Contacts
+                  WHERE MemberID_A = $1
+                  AND MemberID_B = $2`;
+        let values = [request.params.memberID_A, request.params.memberID_B];
+        pool.query(insert, values)
+            .then(result => {
+                response.send({
+                    success: true
+                })
+            }).catch(err => {
+            response.status(400).send({
+                message: "SQL Error",
+                error: err
+            })
+        })
+    }
+);
+
+router.post("/", (request, response, next) => {
+    //validate on empty parameters
+    if (!request.body.memberid_a || !request.body.memberid_b) {
+        response.status(400).send({
+            message: "Missing required information"
+        });
+    } else if (isNaN(request.body.memberid_a) || (isNaN(request.body.memberid_b))) {
+        response.status(400).send({
+            message: "Malformed parameter. member ID must be a number"
+        });
+    } else {
+        next();
+    }
+}, (request, response, next) => {
+    //validate chat id exists
+    let query = 'SELECT * FROM MEMBERS WHERE MEMBERID IN ($1, $2)';
+    let values = [request.body.memberid_a, request.body.memberid_b];
+    pool.query(query, values)
+        .then(result => {
+            if (result.rowCount == 0) {
+                response.status(404).send({
+                    message: "Member not found"
+                });
+            } else {
+                next();
+            }
+        }).catch(error => {
+        response.status(400).send({
+            message: "SQL Error",
+            error: error
+        });
+    });
+}, (request, response, next) => {
+    //add the message to the database
+    let insert = 'INSERT INTO Contacts(MemberID_A, MemberID_B, Verified) VALUES($1, $2, $3)';
+    let values = [request.body.memberid_a, request.body.memberid_b, 1];
+    pool.query(insert, values)
+        .then(result => {
+            if (result.rowCount == 1) {
+                //TODO: Do something here
+                next();
+            } else {
+                response.status(400).send({
+                    "message": "unknown error"
+                });
+            }
+        }).catch(err => {
+        response.status(400).send({
+            message: "SQL Error on insert",
+            error: err
+        });
+    });
+}, (request, response) => {
+    // send a notification of this message to ALL members with registered tokens
+    let query = `SELECT token FROM Push_Token
+                        INNER JOIN Contacts ON
+                        Push_Token.memberid = Contacts.memberid_B
+                        WHERE Contacts.MEMBERID_A=$1`;
+    let values = [request.body.memberid_a];
+    pool.query(query, values)
+        .then(result => {
+            result.rows.forEach(entry =>
+                msg_functions.sendMessageToIndividual(
+                    entry.token,
+                    response.message));
+            response.send({
+                success:true
+            });
+        }).catch(err => {
+        response.status(400).send({
+            message: "SQL Error on select from push token",
+            error: err
+        });
+    });
+});
+
+
 module.exports = router;
--- routes/weather.js
+++ routes/weather.js
@@ -11,51 +11,64 @@ const router = express.Router();
 const TOTAL_HOURLY = 24;
 
 const TOTAL_DAILY = 5;
-const LATITUDE = 47.2451;
-const LONGITUDE = 122.4380;
-
 
 router.get("/", (req, res) => {
     res.type("application/json");
-    let url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + LATITUDE + "&lon=" + LONGITUDE +
-        "&exclude=&appid=" + WEATHER_KEY;
-    let currentData = {};
-    let hourData = {
-        data: []
-    };
-    let dayData = {
-        data: []
-    };
-    request(url, function (error, response, body) {
+    let latitude = "47.2451";
+    let longitude = "122.4380";
+    let zipcode = "98402";
+    if (req.query.zip) {
+        zipcode = req.query.zip;
+    }
+    let geoUrl = "https://geocode.xyz/" + zipcode + "?region=US&json=1&auth=" + process.env.GEOCODE_API_KEY;
+    request(geoUrl, function(error, response, body) {
         if (error) {
             res.send(error);
         } else {
-            let newBody = JSON.parse(body);
-            let current = newBody.current;
-            let daily = newBody.daily;
-            let hourly = newBody.hourly;
+            let geo = JSON.parse(body);
+            latitude = geo.latt;
+            longitude = geo.longt;
+            let url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude +
+                "&exclude=&appid=" + WEATHER_KEY;
+            let currentData = {};
+            let hourData = {
+                data: []
+            };
+            let dayData = {
+                data: []
+            };
+            request(url, function (error, response, body) {
+                if (error) {
+                    res.send(error);
+                } else {
+                    let newBody = JSON.parse(body);
+                    let current = newBody.current;
+                    let daily = newBody.daily;
+                    let hourly = newBody.hourly;
 
-            currentData.temp = current.temp;
-            currentData.weather = current.weather[0].main;
+                    currentData.temp = current.temp;
+                    currentData.weather = current.weather[0].main;
 
-            for (let i = 0; i < TOTAL_HOURLY; i++) {
-                let newEntry = {};
-                newEntry.tempMin = hourly[i].temp;
-                newEntry.weather = hourly[i].weather[0].main;
-                hourData.data.push(newEntry);
-            }
-            for (let i = 0; i < TOTAL_DAILY; i++) {
-                let dayEntry = {};
-                dayEntry.tempMin = daily[i].temp.min;
-                dayEntry.tempMax = daily[i].temp.max;
-                dayEntry.weather = daily[i].weather[0].main;
-                dayData.data.push(dayEntry);
-            }
-            res.status(201).send({
-                current: currentData,
-                hourly: hourData,
-                daily: dayData
-            })
+                    for (let i = 0; i < TOTAL_HOURLY; i++) {
+                        let newEntry = {};
+                        newEntry.tempMin = hourly[i].temp;
+                        newEntry.weather = hourly[i].weather[0].main;
+                        hourData.data.push(newEntry);
+                    }
+                    for (let i = 0; i < TOTAL_DAILY; i++) {
+                        let dayEntry = {};
+                        dayEntry.tempMin = daily[i].temp.min;
+                        dayEntry.tempMax = daily[i].temp.max;
+                        dayEntry.weather = daily[i].weather[0].main;
+                        dayData.data.push(dayEntry);
+                    }
+                    res.status(201).send({
+                        current: currentData,
+                        hourly: hourData,
+                        daily: dayData
+                    })
+                }
+            });
         }
     });
 });
