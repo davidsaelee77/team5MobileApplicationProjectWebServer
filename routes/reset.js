d91f289e82460427541e7e4f090b72ced730c098
patrickmoy <patrickrnmoy@gmail.com>
2020-05-18 20:46:54 -0700

    FUNCTIONAL

--- routes/reset.js
+++ routes/reset.js
@@ -81,11 +81,6 @@ config = {
  */
 router.post('/', (req, res) => {
     res.type("application/json");
-    res.status(200).send({
-        name: req.body.name,
-        pw: req.body.password,
-        confirm: req.body.confirm
-    });
     if (req.body.name && req.body.password && req.body.confirm && req.body.password === req.body.confirm) {
         let decoded = jwt.decode(req.body.name);
         let values = [decoded.email];
