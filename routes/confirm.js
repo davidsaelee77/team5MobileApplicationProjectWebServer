413e26110cd2e9e6bdd42aef6da8a97c515560ae
TylerLorella <lorella.tyler@gmail.com>
2020-05-19 22:30:42 -0700

    completed confirm docs, deleted confirm.get because it was depreciated

--- routes/confirm.js
+++ routes/confirm.js
@@ -41,7 +41,12 @@ config = {
 };
 
 /**
- * @api {get} /confirm?=params verification with parameter
+ * @apiDefine JSONError
+ * @apiError (400: JSON Error) {String} message "malformed JSON in parameters"
+ */
+
+/**
+ * @api {Post} /confirm?=params verification with parameter
  * @apiName PostConfirm
  * @apiGroup Confirm
  *
@@ -55,46 +60,6 @@ config = {
  *
  * @apiError (400: SQL Error) {String} message the reported SQL error details
  */
-router.get("/", (req, res) => {
-    if (req.query.name) {
-        let decoded = jwt.decode(req.query.name);
-        let theQuery = "SELECT MemberID FROM Members WHERE Email = $1 AND VERIFICATION = 0";
-        let values = [decoded.email];
-        pool.query(theQuery, values)
-            .then(result => {
-                if (result.rowCount > 0) {
-                    let updateQuery = "UPDATE Members SET Verification = 1 WHERE Email = $1";
-                    pool.query(updateQuery, values)
-                        .then(result => {
-                            res.status(201).send({
-                                    success: true,
-                                    message: values[0] + " verified!"
-                            });
-                        })
-                        .catch(err => {
-                            res.status(400).send({
-                                message: err.detail
-                            });
-                        })
-                } else {
-                    res.status(400).send({
-                        message: "Unable to verify email currently"
-                    })
-                }
-            })
-            .catch(err => {
-                res.status(400).send({
-                    message: err.detail
-                });
-            });
-        } else {
-        res.status(400);
-        res.send({
-            message: "Invalid verification link"
-        });
-    }
-});
-
 router.post("/", (req, res) => {
     if (req.body.name) {
         console.log(req.body.name);
