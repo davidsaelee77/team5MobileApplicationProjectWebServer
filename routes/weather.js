2b5a19ce34cc2f6a4f3c1e7f942acdd1ceeb7001
patrickmoy <patrickrnmoy@gmail.com>
2020-05-29 01:06:54 -0700

    Weather amended to take either a zip code, or lat/long parameters. Requests w/ zero parameters will fail, and if all are present, zipcode will take priority. Weather will also return city, zipcode, and lat/long (city/zipcode may not be available for certain odd locations, and zipcode is only for the US.) APIDoc needs to be amended to match (TODO).

--- routes/weather.js
+++ routes/weather.js
@@ -48,77 +48,149 @@ const TOTAL_DAILY = 5;
  */
 router.get("/", (req, res) => {
     res.type("application/json");
-    let latitude, longitude;
-    let zipcode = "98402";
     if (req.query.zip && req.query.zip.length === 5) {
         zipcode = req.query.zip;
-    } else if (req.query.latitude && req.query.longitude) {
-
-    }
-    let googleUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=zipcode" + zipcode + "&key=" +
-        process.env.GOOGLE_API_KEY;
-    request(googleUrl, function(error, response, body) {
-        if (error) {
-            res.send(error);
-        } else {
-
-            let googleGeo = JSON.parse(body);
-            latitude = googleGeo.results[0].geometry.location.lat;
-            longitude = googleGeo.results[0].geometry.location.lng;
-            let locationInfo = googleGeo.results[0].address_components;
-            let cityName = "Unknown";
-            for (let i = 0; i < locationInfo.length; i++) {
-                if (locationInfo[i].types.includes("locality") ||
-                    locationInfo[i].types.includes("sublocality") ||
-                    locationInfo[i].types.includes("sublocality_level_1")) {
-                    cityName = locationInfo[i].short_name;
-                    break;
+        let googleUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=zipcode" + zipcode + "&key=" +
+            process.env.GOOGLE_API_KEY;
+        request(googleUrl, function(error, response, body) {
+            if (error) {
+                res.send(error);
+            } else {
+                let googleGeo = JSON.parse(body);
+                let latitude = googleGeo.results[0].geometry.location.lat;
+                let longitude = googleGeo.results[0].geometry.location.lng;
+                let locationInfo = googleGeo.results[0].address_components;
+                let cityName = "Unknown";
+                for (let i = 0; i < locationInfo.length; i++) {
+                    if (locationInfo[i].types.includes("locality") ||
+                        locationInfo[i].types.includes("sublocality") ||
+                        locationInfo[i].types.includes("sublocality_level_1")) {
+                        cityName = locationInfo[i].short_name;
+                        break;
+                    }
                 }
+                let url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude +
+                    "&exclude=&appid=" + WEATHER_KEY;
+                let currentData = {};
+                let hourData = {
+                    data: []
+                };
+                let dayData = {
+                    data: []
+                };
+                request(url, function (error, response, body) {
+                    if (error) {
+                        res.send(error);
+                    } else {
+                        let newBody = JSON.parse(body);
+                        let current = newBody.current;
+                        let daily = newBody.daily;
+                        let hourly = newBody.hourly;
+
+                        currentData.temp = current.temp;
+                        currentData.weather = current.weather[0].main;
+
+                        for (let i = 0; i < TOTAL_HOURLY; i++) {
+                            let newEntry = {};
+                            newEntry.temp = hourly[i].temp;
+                            newEntry.weather = hourly[i].weather[0].main;
+                            hourData.data.push(newEntry);
+                        }
+                        for (let i = 0; i < TOTAL_DAILY; i++) {
+                            let dayEntry = {};
+                            dayEntry.tempMin = daily[i].temp.min;
+                            dayEntry.tempMax = daily[i].temp.max;
+                            dayEntry.weather = daily[i].weather[0].main;
+                            dayData.data.push(dayEntry);
+                        }
+                        res.status(200).send({
+                            zip: zipcode,
+                            city: cityName,
+                            latitude: latitude,
+                            longitude: longitude,
+                            current: currentData,
+                            hourly: hourData,
+                            daily: dayData
+                        })
+                    }
+                });
             }
-            let url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude +
-                "&exclude=&appid=" + WEATHER_KEY;
-            let currentData = {};
-            let hourData = {
-                data: []
-            };
-            let dayData = {
-                data: []
-            };
-            request(url, function (error, response, body) {
-                if (error) {
-                    res.send(error);
-                } else {
-                    let newBody = JSON.parse(body);
-                    let current = newBody.current;
-                    let daily = newBody.daily;
-                    let hourly = newBody.hourly;
-
-                    currentData.temp = current.temp;
-                    currentData.weather = current.weather[0].main;
-
-                    for (let i = 0; i < TOTAL_HOURLY; i++) {
-                        let newEntry = {};
-                        newEntry.temp = hourly[i].temp;
-                        newEntry.weather = hourly[i].weather[0].main;
-                        hourData.data.push(newEntry);
+        });
+    } else if (req.query.latitude && req.query.longitude) {
+        let coords = req.query.latitude + "," + req.query.longitude;
+        let latitude = req.query.latitude;
+        let longitude = req.query.longitude;
+        let googleUrl = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + coords + "&key=" +
+            process.env.GOOGLE_API_KEY;
+        request(googleUrl, function (error, response, body) {
+            if (error) {
+                res.send(error);
+            } else {
+                let zip = "N/A";
+                let cityName = "Unknown";
+                let locationInfo = JSON.parse(body).results[0].address_components;
+                for (let i = 0; i < locationInfo.length; i++) {
+                    if (locationInfo[i].types.includes("locality") ||
+                        locationInfo[i].types.includes("sublocality") ||
+                        locationInfo[i].types.includes("sublocality_level_1")) {
+                        cityName = locationInfo[i].short_name;
                     }
-                    for (let i = 0; i < TOTAL_DAILY; i++) {
-                        let dayEntry = {};
-                        dayEntry.tempMin = daily[i].temp.min;
-                        dayEntry.tempMax = daily[i].temp.max;
-                        dayEntry.weather = daily[i].weather[0].main;
-                        dayData.data.push(dayEntry);
+                    if (locationInfo[i].types.includes("postal_code")) {
+                        zip = locationInfo[i].short_name;
                     }
-                    res.status(200).send({
-                        city: cityName,
-                        current: currentData,
-                        hourly: hourData,
-                        daily: dayData
-                    })
                 }
-            });
-        }
-    });
+                let url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude +
+                    "&exclude=&appid=" + WEATHER_KEY;
+                let currentData = {};
+                let hourData = {
+                    data: []
+                };
+                let dayData = {
+                    data: []
+                };
+                request(url, function (error, response, body) {
+                    if (error) {
+                        res.send(error);
+                    } else {
+                        let newBody = JSON.parse(body);
+                        let current = newBody.current;
+                        let daily = newBody.daily;
+                        let hourly = newBody.hourly;
+
+                        currentData.temp = current.temp;
+                        currentData.weather = current.weather[0].main;
+
+                        for (let i = 0; i < TOTAL_HOURLY; i++) {
+                            let newEntry = {};
+                            newEntry.temp = hourly[i].temp;
+                            newEntry.weather = hourly[i].weather[0].main;
+                            hourData.data.push(newEntry);
+                        }
+                        for (let i = 0; i < TOTAL_DAILY; i++) {
+                            let dayEntry = {};
+                            dayEntry.tempMin = daily[i].temp.min;
+                            dayEntry.tempMax = daily[i].temp.max;
+                            dayEntry.weather = daily[i].weather[0].main;
+                            dayData.data.push(dayEntry);
+                        }
+                        res.status(200).send({
+                            city: cityName,
+                            zip: zip,
+                            latitude: latitude,
+                            longitude: longitude,
+                            current: currentData,
+                            hourly: hourData,
+                            daily: dayData
+                        });
+                    }
+                });
+            }
+        });
+    } else {
+        res.status(400).send({
+            message: "Missing required location info!"
+        });
+    }
 });
 
 module.exports = router;
