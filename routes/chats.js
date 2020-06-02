bdb9f36b47dee7c4bf6cc2b325df2d0390a8d1ca
TylerLorella <lorella.tyler@gmail.com>
2020-06-02 16:41:20 -0700

    modified chat PUT endpoint to instead accept username

--- routes/chats.js
+++ routes/chats.js
@@ -79,19 +79,19 @@ router.post("/", (request, response, next) => {
  * @apiName PutChats
  * @apiGroup Chats
  * 
- * @apiDescription Adds the memberId into the associated chatId. 
+ * @apiDescription Adds the username into the associated chatId. 
  * 
  * @apiHeader {String} authorization Valid JSON Web Token JWT
  * 
  * @apiParam {Number} chatId the chat to add the user to
- * @apiParam {Number} memberId the member to add to the chat
+ * @apiParam {Number} username the member to add to the chat
  * 
  * @apiSuccess {boolean} success true when the name is inserted
  * 
  * @apiError (404: Chat Not Found) {String} message "chatId not found"
- * @apiError (404: Member Not Found) {String} message "memberId not found"
+ * @apiError (404: Member Not Found) {String} message "username not found"
  * @apiError (400: Invalid Parameter) {String} message "Malformed parameter. chatId must be a number"
- * @apiError (400: Invalid Parameter) {String} message "Malformed parameter. memberId must be a number" 
+ * @apiError (400: Invalid Parameter) {String} message "Malformed parameter. username must not be null" 
  * @apiError (400: Duplicate Email) {String} message "user already joined"
  * @apiError (400: Missing Parameters) {String} message "Missing required information"
  * 
@@ -111,7 +111,7 @@ router.put("/", (request, response, next) => {
         });
     } else if (request.query.username == null) {
         response.status(400).send({
-            message: "Malformed parameter. username must be alphanumeric"
+            message: "Malformed parameter. username must not be null"
         });
     } else {
         console.log("pass verify input");
@@ -153,7 +153,7 @@ router.put("/", (request, response, next) => {
         .then(result => {
             if (result.rowCount == 0) {
                 response.status(404).send({
-                    message: "Username not found"
+                    message: "username not found"
                 });
             } else {
                 //user found
